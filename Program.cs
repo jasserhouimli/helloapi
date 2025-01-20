a99acbb6add0c92c03f93a8debd57d078515b845
Jasser Houimli <108799236+jasserhouimli@users.noreply.github.com>
2025-01-20 11:01:19 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -73,7 +73,11 @@ builder.Services.AddSwaggerGen(options =>
 
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated(); // Creates the database and schema
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
