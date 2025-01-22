e7034ebac15d24de69ee9a9d8093e69a6232c4cc
Jasser Houimli <108799236+jasserhouimli@users.noreply.github.com>
2025-01-22 16:36:18 +0100

    Update ProblemsController.cs

--- Controller/ProblemsController.cs
+++ Controller/ProblemsController.cs
@@ -11,7 +11,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 namespace api.Controller
 {
-    [Authorize]
     [ApiController]
     [Route("api/[controller]")]
     public class ProblemsController : ControllerBase
